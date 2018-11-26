5d46e3f1136c6620b29a18b3ebc1cddc197ff6ae
eirikh1996 <eirikhetland1996@gmail.com>
2018-11-26 11:47:55 +0100

    updated some stuff

--- src/main/java/io/github/eirikh1996/nationcraft/config/Settings.java
+++ src/main/java/io/github/eirikh1996/nationcraft/config/Settings.java
@@ -6,6 +6,9 @@ public class Settings {
 	public int maxStrengthPerPlayer = 30;
 	public int maxAllies = -1;
 	public int maxPlayersPerNation = 50;
-	public List<String> forbiddenNationNames = null;
+	public List<String> forbiddenNames = null; //Names that are forbidden to use for nations or settlements
+	public int reduceStrengthOnDeath = 1;
+
+
 	
 }
--- src/main/java/io/github/eirikh1996/nationcraft/listener/PlayerListener.java
+++ src/main/java/io/github/eirikh1996/nationcraft/listener/PlayerListener.java
@@ -1,4 +1,18 @@
 package io.github.eirikh1996.nationcraft.listener;
 
-public class PlayerListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public class PlayerListener implements Listener {
+    @EventHandler
+    public void onLogin(PlayerLoginEvent event){
+
+    }
+
+    @EventHandler
+    public void  onPlayerDeath(PlayerDeathEvent event){
+
+    }
 }
